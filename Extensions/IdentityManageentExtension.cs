14a1bae2093902d735bb865302912d44634fd8ef
Nayanajith Gajasinghe <it18153682@my.sliit.lk>
2022-04-01 18:22:10 +0530

    first commit

---
+++ Extensions/IdentityManageentExtension.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Vertex.RMS.Application.Common.Models;
+
+namespace Vertex.RMS.Application.Common.Extensions;
+
+public static class IdentityManageentExtension
+{
+    public static IResult ToApplicationResult(this IdentityResult result)
+    {
+        return result.Succeeded
+            ? Results.Ok()
+            : Results.BadRequest(Result.Failure(result.Errors.Select(e => e.Description)));
+    }
+
+    public static IResult ToApplicationResult(this SignInResult result)
+    {
+        return result.Succeeded
+            ? Results.Ok()
+            : Results.BadRequest(Result.Failure(new[]
+            {
+                "Invalid email or password",
+                $"{nameof(result.IsLockedOut)}:{result.IsLockedOut}",
+                $"{nameof(result.IsNotAllowed)}:{result.IsNotAllowed}",
+                $"{nameof(result.RequiresTwoFactor)}:{result.RequiresTwoFactor}"
+            }));
+    }
+
+    //public static IResult ToApplicationResponse<T>(this SignInResult result, T data)
+    //{
+    //    return result.Succeeded
+    //        ? Results.Ok(data)
+    //        : Results.BadRequest(Result.Failure(new[]
+    //        {
+    //            "Invalid email or password",
+    //            $"{nameof(result.IsLockedOut)}:{result.IsLockedOut}",
+    //            $"{nameof(result.IsNotAllowed)}:{result.IsNotAllowed}",
+    //            $"{nameof(result.RequiresTwoFactor)}:{result.RequiresTwoFactor}"
+    //        }));
+    //}
+}
